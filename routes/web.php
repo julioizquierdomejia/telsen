2a29f2143a1dfa4f879b1e40c63ce6eceebe23ad
Miguel AB <abm_8249@outlook.com>
2020-10-27 21:54:53 -0500

    auth para rutas

--- routes/web.php
+++ routes/web.php
@@ -21,38 +21,39 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//rutas para ordenes de trabajo
-//Route::resource('ordenes', App\Http\Controllers\OtController::class);
-Route::get('ordenes', [App\Http\Controllers\OtController::class, 'index'])->name('ordenes.index');
-Route::get('ordenes/crear', [App\Http\Controllers\OtController::class, 'create'])->name('ordenes.create');
-Route::post('ordenes', [App\Http\Controllers\OtController::class, 'store'])->name('ordenes.store');
-Route::get('ordenes/{orden}/editar', [App\Http\Controllers\OtController::class, 'edit'])->name('ordenes.edit');
-Route::get('ordenes/{orden}/ver', [App\Http\Controllers\OtController::class, 'show'])->name('ordenes.show');
-Route::post('ordenes/{orden}/editar', [App\Http\Controllers\OtController::class, 'update'])->name('ordenes.update');
+Route::middleware(['auth:' . config('admin-auth.defaults.guard')])->group(function () {
+	//rutas para ordenes de trabajo
+	//Route::resource('ordenes', App\Http\Controllers\OtController::class);
+	Route::get('ordenes', [App\Http\Controllers\OtController::class, 'index'])->name('ordenes.index');
+	Route::get('ordenes/crear', [App\Http\Controllers\OtController::class, 'create'])->name('ordenes.create');
+	Route::post('ordenes', [App\Http\Controllers\OtController::class, 'store'])->name('ordenes.store');
+	Route::get('ordenes/{orden}/editar', [App\Http\Controllers\OtController::class, 'edit'])->name('ordenes.edit');
+	Route::get('ordenes/{orden}/ver', [App\Http\Controllers\OtController::class, 'show'])->name('ordenes.show');
+	Route::post('ordenes/{orden}/editar', [App\Http\Controllers\OtController::class, 'update'])->name('ordenes.update');
 
-Route::get('ordenes/lista', [App\Http\Controllers\OtController::class, 'list'])->name('ordenes.list');
+	Route::get('ordenes/lista', [App\Http\Controllers\OtController::class, 'list'])->name('ordenes.list');
+
+	//rutas para Clientes
+	//Route::resource('clientes', App\Http\Controllers\ClientController::class);
+	Route::get('clientes', [App\Http\Controllers\ClientController::class, 'index'])->name('clientes.index');
+	Route::get('clientes/{client}/ver', [App\Http\Controllers\ClientController::class, 'show'])->name('clientes.ver');
+	Route::get('clientes/crear', [App\Http\Controllers\ClientController::class, 'create'])->name('clientes.create');
+	Route::post('clientes', [App\Http\Controllers\ClientController::class, 'store'])->name('clientes.store');
+	Route::get('clientes/{client}/editar', [App\Http\Controllers\ClientController::class, 'edit'])->name('clientes.edit');
+	Route::post('clientes/{client}/editar', [App\Http\Controllers\ClientController::class, 'update'])->name('clientes.update');
 
 
-//rutas para Clientes
-//Route::resource('clientes', App\Http\Controllers\ClientController::class);
+	//rutas para Marcas
+	Route::get('marcas', [App\Http\Controllers\BrandMotorController::class, 'index'])->name('marcas.index');
+	Route::get('marcas/crear', [App\Http\Controllers\BrandMotorController::class, 'create'])->name('marcas.create');
+	Route::post('marcas', [App\Http\Controllers\BrandMotorController::class, 'store'])->name('marcas.store');
+	Route::get('marcas/{marca}/editar', [App\Http\Controllers\BrandMotorController::class, 'edit'])->name('marcas.edit');
+	Route::post('marcas/{marca}/editar', [App\Http\Controllers\BrandMotorController::class, 'update'])->name('marcas.update');
 
-Route::get('clientes', [App\Http\Controllers\ClientController::class, 'index'])->name('clientes.index');
-Route::get('clientes/{client}/ver', [App\Http\Controllers\ClientController::class, 'show'])->name('clientes.ver');
-Route::get('clientes/crear', [App\Http\Controllers\ClientController::class, 'create'])->name('clientes.create');
-Route::post('clientes', [App\Http\Controllers\ClientController::class, 'store'])->name('clientes.store');
-Route::get('clientes/{client}/editar', [App\Http\Controllers\ClientController::class, 'edit'])->name('clientes.edit');
-Route::post('clientes/{client}/editar', [App\Http\Controllers\ClientController::class, 'update'])->name('clientes.update');
+	Route::get('modelos', [App\Http\Controllers\ModelMotorController::class, 'index'])->name('modelos.index');
+	Route::get('modelos/crear', [App\Http\Controllers\ModelMotorController::class, 'create'])->name('modelos.create');
+	Route::post('modelos', [App\Http\Controllers\ModelMotorController::class, 'store'])->name('modelos.store');
+	Route::get('modelos/{marca}/editar', [App\Http\Controllers\ModelMotorController::class, 'edit'])->name('modelos.edit');
+	Route::post('modelos/{marca}/editar', [App\Http\Controllers\ModelMotorController::class, 'update'])->name('modelos.update');
 
-
-//rutas para Marcas
-Route::get('marcas', [App\Http\Controllers\BrandMotorController::class, 'index'])->name('marcas.index');
-Route::get('marcas/crear', [App\Http\Controllers\BrandMotorController::class, 'create'])->name('marcas.create');
-Route::post('marcas', [App\Http\Controllers\BrandMotorController::class, 'store'])->name('marcas.store');
-Route::get('marcas/{marca}/editar', [App\Http\Controllers\BrandMotorController::class, 'edit'])->name('marcas.edit');
-Route::post('marcas/{marca}/editar', [App\Http\Controllers\BrandMotorController::class, 'update'])->name('marcas.update');
-
-Route::get('modelos', [App\Http\Controllers\ModelMotorController::class, 'index'])->name('modelos.index');
-Route::get('modelos/crear', [App\Http\Controllers\ModelMotorController::class, 'create'])->name('modelos.create');
-Route::post('modelos', [App\Http\Controllers\ModelMotorController::class, 'store'])->name('modelos.store');
-Route::get('modelos/{marca}/editar', [App\Http\Controllers\ModelMotorController::class, 'edit'])->name('modelos.edit');
-Route::post('modelos/{marca}/editar', [App\Http\Controllers\ModelMotorController::class, 'update'])->name('modelos.update');
+});

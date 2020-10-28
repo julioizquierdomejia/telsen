b880b52c715d64b02e92c40e5f163ded1ab02b53
Miguel AB <abm_8249@outlook.com>
2020-10-28 10:31:24 -0500

    agregar autorizacion por rol en home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,8 +22,10 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
+        $request->user()->authorizeRoles(['superadmin', 'admin']);
+
         $usuario = User::all();
         return view('home', compact('usuario'));
     }

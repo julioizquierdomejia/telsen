581d7fb381c1ae358df8a70823d60e2d418b4af4
Miguel AB <abm_8249@outlook.com>
2020-11-03 21:28:49 -0500

    validar para mostrar evaluacion

--- resources/views/formatos/mechanical/show.blade.php
+++ resources/views/formatos/mechanical/show.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app', ['title' => 'Ver Evaluación Mecánica'])
 @section('content')
+@php $data = $formato->toArray() @endphp
 <div class="row">
      <div class="col-md-12">
-          @foreach($formato->toArray() as $key => $item)
-          <p><strong>{{$key}}</strong>: {{$item}}</p>
+          @foreach($data as $key => $item)
+          <p><strong>{{ucwords(str_replace("_", " ", $key))}}</strong>: {{$item}}</p>
           @endforeach
      </div>
 </div>

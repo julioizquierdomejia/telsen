f120012c6edd4c97f29006d09a5669a0586a7d56
Miguel AB <abm_8249@outlook.com>
2020-11-20 00:30:15 -0500

    campo maquina

--- app/Http/Controllers/MechanicalEvaluationController.php
+++ app/Http/Controllers/MechanicalEvaluationController.php
@@ -74,7 +74,7 @@ class MechanicalEvaluationController extends Controller
             'hp_kw' => 'required',
 
             'serie' => 'string|nullable',
-            //'solped' => 'string|nullable',
+            'maquina' => 'string|nullable',
             'placa_caract_orig' => 'string|nullable',
             'tapas' => 'string|nullable',
             'ventilador' => 'string|nullable',
@@ -149,7 +149,7 @@ class MechanicalEvaluationController extends Controller
         $meval->hp_kw = $request->input('hp_kw');
 
         $meval->serie = $request->input('serie');
-        //$meval->solped = $request->input('solped');
+        $meval->maquina = $request->input('maquina');
         $meval->placa_caract_orig = $request->input('placa_caract_orig');
         $meval->tapas = $request->input('tapas');
         $meval->ventilador = $request->input('ventilador');
@@ -344,7 +344,7 @@ class MechanicalEvaluationController extends Controller
         $meval->rpm = $request->input('rpm');
         $meval->hp_kw = $request->input('hp_kw');
         $meval->serie = $request->input('serie');
-        //$meval->solped = $request->input('solped');
+        $meval->maquina = $request->input('maquina');
         $meval->placa_caract_orig = $request->input('placa_caract_orig');
         $meval->tapas = $request->input('tapas');
         $meval->ventilador = $request->input('ventilador');

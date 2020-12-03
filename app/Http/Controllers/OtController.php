851dbab53cddd36e75eced8371b1bb168f42f054
Miguel AB <abm_8249@outlook.com>
2020-12-03 00:51:56 -0500

    permiso a aprobador de evaluaciones a listado de ots

--- app/Http/Controllers/OtController.php
+++ app/Http/Controllers/OtController.php
@@ -38,11 +38,11 @@ class OtController extends Controller
 
     public function enabled_ots(Request $request)
     {
-        $request->user()->authorizeRoles(['superadmin', 'admin', 'evaluador', 'crear_ot']);
+        $request->user()->authorizeRoles(['superadmin', 'admin', 'evaluador', 'aprobador_de_evaluaciones', 'crear_ot']);
 
         $role_names = validateActionbyRole();
         $admin = in_array("superadmin", $role_names) || in_array("admin", $role_names);
-        $evaluador = in_array("evaluador", $role_names);
+        $evaluador = in_array("evaluador", $role_names) || in_array("aprobador_de_evaluaciones", $role_names);
         $allowed_users = $admin || $evaluador;
 
         $counter = 0;

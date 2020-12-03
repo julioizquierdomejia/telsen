928cd4b265e53f0f28ad51699c8f4f0ba87d3c1f
Miguel AB <marteaga@grupohardtech.com>
2020-12-03 18:39:05 -0500

    ordenar botones de lista de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,7 +79,7 @@ class UserController extends Controller
             $email = $record->email;
             $enabled = ($record->enabled == 1 ? '<span class="badge badge-success d-block">Activo</span>' : '<span class="badge badge-secondary d-block">Inactivo</span>');
 
-            $tools = ($record->enabled == 1 ? '<button type="button" class="btn btn-sm btn-danger btn-mdelete" data-userid="'.$record->id.'" data-state="0" data-toggle="modal" data-target="#modalUser" title="Desactivar usuario"><i class="fal fa-trash"></i></button>' : '<button type="button" class="btn btn-sm btn-primary btn-mdelete" data-userid="'.$record->id.'" data-state="1" data-toggle="modal" data-target="#modalUser" title="Restaurar usuario"><i class="fal fa-trash-undo-alt"></i></button>').'<a href="/usuario/'.$record->id.'/perfil" class="btn btn-sm btn-warning"><i class="fal fa-edit"></i></a>';
+            $tools = '<a href="/usuario/'.$record->id.'/perfil" class="btn btn-sm btn-warning"><i class="fal fa-edit"></i></a> '.($record->enabled == 1 ? '<button type="button" class="btn btn-sm btn-danger btn-mdelete" data-userid="'.$record->id.'" data-state="0" data-toggle="modal" data-target="#modalUser" title="Desactivar usuario"><i class="fal fa-trash"></i></button>' : '<button type="button" class="btn btn-sm btn-primary btn-mdelete" data-userid="'.$record->id.'" data-state="1" data-toggle="modal" data-target="#modalUser" title="Restaurar usuario"><i class="fal fa-trash-undo-alt"></i></button>');
 
             $data_arr[] = array(
               "id" => $counter,

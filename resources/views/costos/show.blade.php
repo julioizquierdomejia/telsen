574e4a3b436902785f88b6d2ee9d967ea15de3d5
Miguel AB <abm_8249@outlook.com>
2020-12-04 10:54:10 -0500

    corregir error de mensaje de aprobacion de cotizacion

--- resources/views/costos/show.blade.php
+++ resources/views/costos/show.blade.php
@@ -462,15 +462,23 @@ $rol_fecha = in_array("fecha_de_entrega", $role_names);
 			@endif
 			@if(in_array("cc_approved", $statuses))
 				<div class="text-center py-3 row">
+					@if ($maded_by)
 					<div class="col">Hecha por: {{$maded_by->name}}</div>
+					@endif
 					<span class="badge badge-success px-3 py-2">Cotización Aprobada</span>
+					@if ($approved_by)
 					<div class="col ml-auto">Aprobada por: {{$approved_by->name}}</div>
+					@endif
 				</div>
 				@elseif(in_array("cc_disapproved", $statuses))
 				<div class="text-center py-3 row">
+					@if ($maded_by)
 					<div class="col">Hecha por: {{$maded_by->name}}</div>
+					@endif
 					<span class="badge badge-success px-3 py-2">Cotización Desaprobada</span>
+					@if ($approved_by)
 					<div class="col ml-auto">Desaprobada por: {{$approved_by->name}}</div>
+					@endif
 				</div>
 				@endif
 			<embed class="w-100" src="/uploads/cotizacion/{{$ccost->cotizacion}}" width="500" height="375" style="height: calc(100vh - 140px)" type="application/pdf">

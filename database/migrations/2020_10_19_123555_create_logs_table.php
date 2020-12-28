e8a8ad73298a95a3ac2779d3d077a197b54accaf
Miguel AB <abm_8249@outlook.com>
2020-12-28 00:51:23 -0500

    cascade para logs de usuarios

--- database/migrations/2020_10_19_123555_create_logs_table.php
+++ database/migrations/2020_10_19_123555_create_logs_table.php
@@ -17,7 +17,7 @@ class CreateLogsTable extends Migration
             $table->id();
             
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->string('section')->nullable();
             $table->string('action')->nullable();

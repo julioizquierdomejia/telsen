8573f35926fe62296f00d93d25089300310453f6
Miguel AB <abm_8249@outlook.com>
2021-01-08 00:17:04 -0500

    codigos de rotores

--- database/seeders/RotorCodRodajeSeeder.php
+++ database/seeders/RotorCodRodajeSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Seeder;
 use App\Models\RotorCodRodajePt1;
 use App\Models\RotorCodRodajePt2;
 
+use Illuminate\Support\Facades\DB;
+
 class RotorCodRodajeSeeder extends Seeder
 {
     /**
@@ -13,36 +15,53 @@ class RotorCodRodajeSeeder extends Seeder
      *
      * @return void
      */
+    function insertquote($value) {
+        return "'$value'";
+    }
+
     public function run()
     {
-        //RotorCodRodajeP1
-        $status = new RotorCodRodajePt1();
-        $status->name = '6317 C3';
-        $status->asiento_rodaje = 'Ø INT 85';
-        $status->alojamiento_rodaje = 'Ø EXT 180';
-        $status->enabled = 1;
-        $status->save();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        RotorCodRodajePt1::truncate();
+        RotorCodRodajePt2::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        $status = new RotorCodRodajePt1();
-        $status->name = '6314 C3';
-        $status->asiento_rodaje = 'Ø INT 70';
-        $status->alojamiento_rodaje = 'Ø EXT 150';
-        $status->enabled = 1;
-        $status->save();
+        $lineNumber = 1;
+        if (($handle = fopen(base_path("public/rotor_cod_rodaje_pt.csv"), "r")) !== false) {
+            while (($data = fgets($handle)) !== false) {
+                if ($lineNumber === 1) {
+                    $lineNumber++;
+                    continue;
+                }
+                $lineNumber++;
 
-        //RotorCodRodajeP2
-        $status = new RotorCodRodajePt2();
-        $status->name = '7322 BM';
-        $status->asiento_rodaje = 'Ø INT 110';
-        $status->alojamiento_rodaje = 'Ø EXT 240';
-        $status->enabled = 1;
-        $status->save();
+                $row = str_getcsv($data, ";");
 
-        $status = new RotorCodRodajePt2();
-        $status->name = 'UN 222 ECP C3';
-        $status->asiento_rodaje = 'Ø INT 110';
-        $status->alojamiento_rodaje = 'Ø EXT 200';
-        $status->enabled = 1;
-        $status->save();
+                /*//Tabla 1
+                $createQuery = "INSERT INTO rotor_cod_rodaje_pt1s (name, asiento_rodaje, alojamiento_rodaje) VALUES (".self::insertquote($row[0]).", ".self::insertquote($row[1]).", ".self::insertquote($row[2]).")";
+                DB::statement($createQuery, $row);
+
+                //Tabla 2
+                $createQuery = "INSERT INTO rotor_cod_rodaje_pt2s (name, asiento_rodaje, alojamiento_rodaje) VALUES (".self::insertquote($row[0]).", ".self::insertquote($row[1]).", ".self::insertquote($row[2]).")";
+                DB::statement($createQuery, $row);*/
+
+                $rotor_cod_rodaje_pt1 = new RotorCodRodajePt1;
+                $rotor_cod_rodaje_pt1->name = $row[0];
+                $rotor_cod_rodaje_pt1->asiento_rodaje = $row[1];
+                $rotor_cod_rodaje_pt1->alojamiento_rodaje = $row[2];
+                $rotor_cod_rodaje_pt1->save();
+
+                $rotor_cod_rodaje_pt2 = new RotorCodRodajePt2;
+                $rotor_cod_rodaje_pt2->name = $row[0];
+                $rotor_cod_rodaje_pt2->asiento_rodaje = $row[1];
+                $rotor_cod_rodaje_pt2->alojamiento_rodaje = $row[2];
+                $rotor_cod_rodaje_pt2->save();
+
+                //$this->command->info($row);
+            }
+            fclose($handle);
+        }
+
+
     }
 }
